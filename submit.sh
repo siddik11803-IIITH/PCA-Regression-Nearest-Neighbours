a0a88c4b061cb96cdc6af384ce5c2b5b73874408
siddik11803-IIITH <siddik.naraharisetti@students.iiit.ac.in>
2022-11-01 21:00:19 +0530

    2 got nice values

---
+++ submit.sh
@@ -0,0 +1,4 @@
+cp ./2020101089_Q1.ipynb ./../2020101089_Assignment2
+cp ./2020101089_Q2.ipynb ./../2020101089_Assignment2
+cp ./2020101089_Q3.ipynb ./../2020101089_Assignment2
+cp ./2020101089_Q4.ipynb ./../2020101089_Assignment2
